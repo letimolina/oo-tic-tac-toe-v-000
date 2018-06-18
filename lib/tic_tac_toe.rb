cb74b448cb913454e1e73ced55bf84d654d44f2d
Leticia Molina <letimolina@gmail.com>
2018-06-18 22:42:42 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,10 +3,6 @@ class TicTacToe
     @board = board || Array.new(9, " ")
   end
 
-  # def initialize
-  #   @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  # end
-
   WIN_COMBINATIONS = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8],
     [0, 3, 6], [1, 4, 7], [2, 5, 8],
